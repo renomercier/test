fd59a16136a5e4f3584e7fe1ee03e5403eb13860
Mercier <e1795113@b1128-08.local>
2018-02-06 15:46:12 -0500

    Deuxieme commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-{
-    "fonts.fontSize": "12px",
-    "fonts.fontFamily": "'SourceCodePro-Medium', ＭＳ ゴシック, 'MS Gothic', monospace",
-    "brackets-eslint.gutterMarks": true,
-    "brackets-eslint.useLocalESLint": false
-}
+<?php 
+
+
+echo "Salut a tous, je quitte pour la journee!!";
+
+?>
