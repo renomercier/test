eeb1e2e8e12d3949ba6fc72b638f1120aa8afcb9
Mercier <e1795113@b1128-08.local>
2018-02-06 15:00:45 -0500

    Premier commit

---
+++ index.php
@@ -0,0 +1,6 @@
+{
+    "fonts.fontSize": "12px",
+    "fonts.fontFamily": "'SourceCodePro-Medium', ＭＳ ゴシック, 'MS Gothic', monospace",
+    "brackets-eslint.gutterMarks": true,
+    "brackets-eslint.useLocalESLint": false
+}
